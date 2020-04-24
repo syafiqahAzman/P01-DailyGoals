62333f089a053788941f0ad6053f37bcb888162f
16046491 <16046491@myrp.edu.sg>
2020-04-24 16:21:17 +0800

    Edit the errors

--- app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p01_dailygoals/MainActivity.java
@@ -21,20 +21,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnOk = findViewById(R.id.button);
-        rg1 = findViewById(R.id.rg1);
-        rg2 = findViewById(R.id.rg2);
-        rg3 = findViewById(R.id.rg3);
-        int sb1 = rg1.getCheckedRadioButtonId();
-        int sb2 = rg2.getCheckedRadioButtonId();
-        int sb3 = rg3.getCheckedRadioButtonId();
-        rb1 = findViewById(sb1);
-        rb2 = findViewById(sb2);
-        rb3 = findViewById(sb3);
-        et1 = findViewById(R.id.editText);
+
 
        btnOk.setOnClickListener(new View.OnClickListener() {
            @Override
            public void onClick(View v) {
+
+               rg1 = findViewById(R.id.rg1);
+               rg2 = findViewById(R.id.rg2);
+               rg3 = findViewById(R.id.rg3);
+               int sb1 = rg1.getCheckedRadioButtonId();
+               int sb2 = rg2.getCheckedRadioButtonId();
+               int sb3 = rg3.getCheckedRadioButtonId();
+               rb1 = findViewById(sb1);
+               rb2 = findViewById(sb2);
+               rb3 = findViewById(sb3);
+               et1 = findViewById(R.id.editText);
+               
                //Put the text in an array
                String[] info = {rb1.getText().toString(), rb2.getText().toString()
                        , rb3.getText().toString(), et1.getText().toString()};
